39e7be6b35f338ff7266488c52ecdb6a1f101fbb
Michael Miretsky <michael.miretsky@healthee.co>
2022-12-19 03:57:40 +0200

    remove dist

--- dist/fetch.d.ts
+++
@@ -1,6 +0,0 @@
-import { IPayload } from '.';
-import Base from './base.js';
-export default class Fetch extends Base {
-    end(): Promise<Response>;
-    parse(re: Response): Promise<IPayload>;
-}
